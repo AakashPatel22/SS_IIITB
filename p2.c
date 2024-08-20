46db398bd47172bf9961bac6882f0c38065401b0
Aakash <aakashpatel222222@gmail.com>
2024-08-20 18:00:38 +0530

    Program 2

---
+++ p2.c
@@ -0,0 +1,11 @@
+/*
+Name: Aakash Bhavesh Patel
+Roll No.: MT2024109
+Problem 2, Write a simple program to execute in an infinite loop at the background. Go to /proc directory and
+identify all the process related information in the corresponding proc directory.
+*/
+#include<stdio.h>
+void main(){
+while(1){
+}
+}
